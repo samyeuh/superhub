59796b0d877880ff0a8cba563297902c31909742
Samy <samy.bouhamidi@efrei.net>
2024-11-20 14:38:21 +0100

    chat update

--- src/main/java/com/samy/superhub/SuperHubPlugin.java
+++ src/main/java/com/samy/superhub/SuperHubPlugin.java
@@ -1,6 +1,7 @@
 package com.samy.superhub;
 
 import com.samy.superhub.actionbar.ActionBarListener;
+import com.samy.superhub.chat.ChatListener;
 import com.samy.superhub.hotbar.HotbarListener;
 import com.samy.superhub.spawn.SpawnCommand;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,14 +10,32 @@ public class SuperHubPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getCommand("spawn").setExecutor(new SpawnCommand());
-        getServer().getPluginManager().registerEvents(new HotbarListener(), this);
-        getServer().getPluginManager().registerEvents(new ActionBarListener(this), this);
+        enableSpawn();
+        enableHotBar();
+        enableActionBar();
+        enableChat();
+
         getLogger().info("SuperHubPlugin est activé !");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("SuperHubPlugin est désactivé.");
+    }
+
+    private void enableSpawn(){
+        getCommand("spawn").setExecutor(new SpawnCommand());
+    }
+
+    private void enableHotBar(){
+        getServer().getPluginManager().registerEvents(new HotbarListener(), this);
+    }
+
+    private void enableActionBar(){
+        getServer().getPluginManager().registerEvents(new ActionBarListener(this), this);
+    }
+
+    private void enableChat(){
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
     }
 }
---
+++ src/main/java/com/samy/superhub/chat/ChatListener.java
@@ -0,0 +1,18 @@
+package com.samy.superhub.chat;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event){
+        String playerName = event.getPlayer().getDisplayName();
+        String message = event.getMessage();
+        event.setFormat(playerName + ": " + message);
+
+
+    }
+}
