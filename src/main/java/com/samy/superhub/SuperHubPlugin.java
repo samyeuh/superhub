faafc0e33a6b4c1d1db161c187488dffd24afdf5
samyeuh <samybouhamidi2@gmail.com>
2024-12-11 10:39:45 +0100

    SuperAPI

--- src/main/java/com/samy/superhub/SuperHubPlugin.java
+++ src/main/java/com/samy/superhub/SuperHubPlugin.java
@@ -1,27 +1,26 @@
 package com.samy.superhub;
 
+import com.samy.api.SuperAPI;
 import com.samy.superhub.actionbar.ActionBarListener;
 import com.samy.superhub.chat.ChatListener;
 import com.samy.superhub.hotbar.HotbarListener;
 import com.samy.superhub.interactions.InteractionsListener;
 import com.samy.superhub.spawn.SpawnCommand;
 import com.samy.superhub.spawn.SpawnListener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 public class SuperHubPlugin extends JavaPlugin {
 
-    private Plugin corePlugin;
+    private SuperAPI corePlugin;
     private Map<UUID, List<UUID>> friends;
 
     @Override
     public void onEnable() {
-        corePlugin = getServer().getPluginManager().getPlugin("SuperServeurPlugin");
+        corePlugin = (SuperAPI) getServer().getPluginManager().getPlugin("SuperServeurPlugin");
         if(corePlugin == null){
             getLogger().info("SuperServeur n'est pas activé !");
         } else {
@@ -33,6 +32,7 @@ public class SuperHubPlugin extends JavaPlugin {
         enableActionBar();
         enableChat();
         enableInteractions();
+        enableRankTab();
 
         getLogger().info("SuperHubPlugin est activé !");
     }
@@ -64,13 +64,11 @@ public class SuperHubPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new InteractionsListener(), this);
     }
 
+    private void enableRankTab(){
+        corePlugin.createRankTab();
+    }
+
     public void refreshFriends() {
-        try {
-            Method getFriends = corePlugin.getClass().getMethod("getFriends");
-            friends = (Map<UUID, List<UUID>>) getFriends.invoke(corePlugin);
-        } catch (Exception e) {
-            getLogger().warning("Impossible de récupérer la liste d'amis.");
-            throw new RuntimeException(e);
-        }
+        friends = corePlugin.getFriends();
     }
 }
